014756b07b9cfc5c2d27f65a798108031678403d
Popo-Jin <jjuwa93@naver.com>
2022-05-30 12:53:08 +0900

    220530_12:52_2789_progrssing

--- Main.java
+++ Main.java
@@ -3,7 +3,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,8 +12,19 @@ public class Main {
         StringTokenizer st = new StringTokenizer(s);
         int num = Integer.parseInt(st.nextToken());
         int max = Integer.parseInt(st.nextToken());
+        int max_result = 0;
         String arr[] = br.readLine().split(" ");
-
+        for(int i=0;i<arr.length;i++) {
+            for (int j = i+1; j < arr.length; j++) {
+                for (int k = j+1; k < arr.length; k++) {
+                    int result = Integer.parseInt(arr[i])+Integer.parseInt(arr[j])+Integer.parseInt(arr[k]);
+                    if(result > max_result && result <= max ) {
+                        max_result = result;
+                    }
+                }
+            }
+        }
+        System.out.println(max_result);
         // System.out.println(num + " " + max);
         // System.out.println(Arrays.toString(arr));
     }
