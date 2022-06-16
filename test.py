33e2a27d19b2bab7434977257fee1645bb4a5329
Popo-Jin <jjuwa93@naver.com>
2022-06-16 13:55:26 +0900

    220616_13:55_lotto_ing

--- test.py
+++ test.py
@@ -1,21 +1,88 @@
-from random import randint
+def solution(lottos, win_nums):
+    cnt = 0
+    zero = 0
 
-a = [0,1,2,3,4,5]
-b = []
-for _ in range(0,100):
-    b.append(f"{randint(0,5)} {randint(0,5)}") 
-print(b)
-arr = set(b)
-print(arr)
-id_dict = {id:[] for id in a}
-cnt = {cnt:[] for cnt in a}
+    for i in lottos:
+        if i in win_nums:
+            cnt += 1
+        if i == 0:
+            zero += 1
+    # answer = []
+    # return answer
+    return cnt, zero
+
+lottos = [6, 44, 2, 18, 40, 39]
+win_nums = [2, 4, 16, 19, 26, 39]
+
+print(solution(lottos, win_nums))
+
+# def solution(id_list, report, k):
+#     # 계정별 신고한 계정 목록 딕셔너리 생성(id_dict)
+#     # 계정별 신고당한 횟수 딕셔너리 생성(abuse_cnt)
+#     id_dict = {id: [] for id in id_list}
+#     abuse_cnt_dict = {cnt: 0 for cnt in id_list}
+
+#     # 중복 없애기 ex)입출력 예#2
+#     # new_report = [] 
+#     # for i in report:
+#     #     if i not in new_report:
+#     #         new_report.append(i)
+#     new_report = set(report)
+#     print(new_report)
+#     # 계정별 신고한 계정 딕셔너리 작성
+#     # 계정별 신고당한 횟수 딕셔너리 작성
+#     for i in new_report:
+#         reporter, abuser = i.split()
+#         id_dict[reporter].append(abuser)
+#         abuse_cnt_dict[abuser] += 1
+#     print(id_dict)
+#     print(abuse_cnt_dict)
+#     # 신고당한 횟수가 k 이상인 계정 찾기
+#     find_abuser = []
+#     for id in abuse_cnt_dict:
+#         if abuse_cnt_dict[id] >= k:
+#             find_abuser.append(id)
+
+#     # 계정당 메일 발송해야하는 횟수 구하기
+#     answer = []
+#     for user in id_dict:
+#         cnt = 0
+#         for i in find_abuser:
+#             if i in id_dict[user]:
+#                 cnt += 1
+#         answer.append(cnt)
+#     return answer
+
+# id_list = ["a","b","c","d","e"]
+# report = ["a b","a c","b d","b c","b e","c e","d e","a b","b d"]
+# # id_list = ['1','2','3','4','5']
+# # report = ["1 2","2 3","1 3","1 2","2 4","3 5","2 5","3 5"]
+# solution(id_list,report,2)
+
+# from random import randint
+# a = [0,1,2,3,4,5]
+# b = []
+# for _ in range(0,100):
+#     b.append(f"{randint(0,5)} {randint(0,5)}") 
+# print(b)
+# arr = set(b)
+# print(arr)
+# id_dict = {id:[] for id in a}
+# cnt = {cnt:[] for cnt in a}
 # print(len(arr))
 # print(arr.pop().split(" "))
-a1, a2 = arr.pop().split(" ")
-print(a1,"and",a2)
+
+# print(id_dict)
+# print(cnt)
+# print(reporter)
+# print(a)
+# for i in arr:
+#     reporter, abuser = i.split()
+#     id_dict[id].append(abuser)
+    # cnt[abuser] += 1
 # for i in range(0, len(arr)):
 #     set_id, set_cnt = arr[i].split(" ")
-print(id_dict)
+# print(id_dict)
 
 
 # from collections import deque
