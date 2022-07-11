cddc5bebb8bedba779c9a874bd3ebe8e4e6c5387
unknown <jjuwa93@naver.com>
2022-07-12 00:54:52 +0900

    good

--- 자료구조_course/401-연속합2.py
+++ 자료구조_course/401-연속합2.py
@@ -1,41 +1,25 @@
-'''
-10
-10 -4 3 1 5 6 -35 12 21 -1
-54
-'''
+import sys
 
-n = int(input())
-m = [0]*n
-m = list(map(int, input().split()))
-f = []
-g = []
-check = 0
-result = 0
-result_no = 0
-result_yes = 0
-for i in range(len(m)):
-    if m[i] >= 0:
-        result += m[i]
-    else:
-        f.append(result)
-        f.append(m[i])
-        result = 0
+if __name__ == '__main__':
+    N = int(input())
+    arr = list(map(int, sys.stdin.readline().split()))
+    dp = [[0] * N for _ in range(2)]
+    # dp[0][i] : 특정 원소를 제거하지 않은 경우
+    # dp[1][i] : 특정 원소를 제거한 경우
 
-for i in range(len(f)):
-    if f[i] >= 0:
-        result_no += f[i]
-        result_yes += f[i]
-    else:
-        g.append(result_no)
-        result_no = 0
-        if check == 0:
-            check = 1
-        else:
-            g.append(result_yes)
-            result_yes = 0
-            check = 0
+    dp[0][0] = arr[0] # 1개는 반드시 선택해야 한다.
 
-for i in range(len(g)):
-	result = max(g[i], result)
-
-print(result)
+    if N > 1:
+        ans = -1e9
+        for i in range(1, N):
+            # 아무런 원소를 제거하지 않았을 때, (이전까지의 연속합 + i번쨰 원소) 와 (i번째 원소)를 비교하여 큰 경우를 대입
+            dp[0][i] = max(dp[0][i 
+            - 1] + arr[i], arr[i])
+            # 특정 원소를 제거하는 경우 => 1. i번째 원소를 제거하는 경우 2. i번째 이전에서 이미 특정 원소를 제거하여 i번째 원소를 선택하는 경우
+            # 1의 경우 dp[0][i - 1] 2의 경우 dp[1][i-1] + arr[i]
+            dp[1][i] = max(dp[0][i - 1], dp[1][i-1] + arr[i])
+            # dp 진행 중 가장 큰 값으로 갱신
+            ans = max(ans, dp[0][i], dp[1][i])
+        print(ans)
+    else: # N이 1인 경우는 반드시 선택을 해야하므로 dp[0][0] 출력
+        print(dp[0][0])
---
+++ 자료구조_course/500-일곱난쟁이.py
@@ -0,0 +1,23 @@
+n = [int(input()) for _ in range(9)]
+
+total = sum(n)
+
+for i in range(len(n)):
+    for j in range(i+1, len(n)):
+            tmp = total - (n[i] + n[j])
+            num1, num2 = n[i], n[j]
+            if tmp == 100:
+                n.remove(num1)
+                n.remove(num2)
+                n.sort()
+
+                for i in range(len(n)):
+                    print(n[i])
+                break
+
+
+
+
+
+
+
