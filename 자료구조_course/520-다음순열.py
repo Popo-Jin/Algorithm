c7224a049d33fc05669ca8ee3aabe529f8148ca1
unknown <jjuwa93@naver.com>
2022-07-14 01:59:47 +0900

    good

--- 자료구조_course/520-다음순열.py
+++ 자료구조_course/520-다음순열.py
@@ -1,5 +1,19 @@
+'''
+4
+1 2 3 4
+'''
+
 n = int(input())
 
 m = list(map(int, input().split()))
 
-print(n, m)
+for i in range(n-1, 0, -1):
+    if m[i-1] < m[i]:
+        for j in range(n-1, 0, -1):
+            if m[i-1] < m[j]:
+                m[i-1], m[j] = m[j], m[i-1]
+                m = m[:i] + sorted(m[i:])
+                print(*m)
+                exit()
+
+print(-1)
